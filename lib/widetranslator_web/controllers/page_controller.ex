9daebc859f0cc3a8bf018e773170a5e111704d3b
carlos <clagccs@gmail.com>
2018-11-04 16:41:26 -0400

    revisado error lectura archivo estatico

--- lib/widetranslator_web/controllers/page_controller.ex
+++ lib/widetranslator_web/controllers/page_controller.ex
@@ -5,6 +5,7 @@ defmodule WidetranslatorWeb.PageController do
     #render conn, "index.html"
     #html(conn, File.read!("priv/static/index.html"))
     #revisando ruta
-    html(conn, File.read!(Application.app_dir(:widetranslator,"priv/static/index.html")))
+    #html(conn, File.read!(Application.app_dir(:widetranslator,"priv/static/index.html")))
+    File.stream!(Path.join(:code.priv_dir(:widetranslator), "index.html"))
   end
 end
