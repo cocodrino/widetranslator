c5b598f786342bbf4d93414c5ac99f219655ac56
carlos <clagccs@gmail.com>
2018-11-04 16:44:00 -0400

    revisado error lectura archivo estatico

--- lib/widetranslator_web/endpoint.ex
+++ lib/widetranslator_web/endpoint.ex
@@ -10,7 +10,7 @@ defmodule WidetranslatorWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :widetranslator, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt dist _nuxt about logo.png)
+    only: ~w(css fonts images js favicon.ico robots.txt dist _nuxt about logo.png index.html)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
