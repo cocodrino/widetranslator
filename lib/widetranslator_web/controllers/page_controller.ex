74140687b5a78e88e3b36601f7744c84ef992fe7
carlos <clagccs@gmail.com>
2018-11-03 21:41:58 -0400

    deploy heroku

--- lib/widetranslator_web/controllers/page_controller.ex
+++ lib/widetranslator_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule WidetranslatorWeb.PageController do
   use WidetranslatorWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    #render conn, "index.html"
+    #html(conn, File.read!("priv/static/index.html"))
+    html(conn, File.read!(Application.app_dir(:widetranslator,"priv/static/index.html")))
   end
 end
